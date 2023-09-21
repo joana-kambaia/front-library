936c3bfc9fae694cb3f04670b8dc7fe217a17809
Kambaia Alberto <kambaiaalberto@gmail.com>
2023-09-21 22:02:57 +0100

    initial project

---
+++ src/app/book/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="banner">
+        <h3 className='bg-[#f0f]'>Livro</h3>
+    </main>
+  )
+}
---
+++ src/app/books/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className="banner">
+        <h3 className='bg-green-950 text-white'>Todos os livros</h3>
+    </main>
+  )
+}
